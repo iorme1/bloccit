1391a47b72b1f8510681be2e25ab85f271e99bd2
Isaac Orme <isorme1@gmail.com>
2017-08-10 13:11:22 -0700

    assignment-seeding-data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,17 @@ posts = Post.all
 #Create Comments
 100.times do
   Comment.create!(
-    post: posts.sample
+    post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
+
+puts "#{Post.count}"
+Post.find_or_create_by!(title: "I am a Unique snowflake", body: "Me too!, I'm a snowflake")
+puts "#{Post.count}"
+
+Comment.find_or_create_by!(body: "Unique comment here")
+
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
