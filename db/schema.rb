be6f1fca6d4388ee0788b2669d798c6935b5b433
Isaac Orme <isorme1@gmail.com>
2017-08-11 16:40:44 -0700

    assignment-crud: completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809214328) do
+ActiveRecord::Schema.define(version: 20170811220732) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
---
+++ spec/factories/advertisements.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :advertisement do
+    title "MyString"
+    body "MyText"
+    price 1
+  end
+end
