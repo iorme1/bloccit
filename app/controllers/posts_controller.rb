7e52f6517d630d3b09bfd83a112a247db91030e3
Isaac Orme <isorme1@gmail.com>
2017-08-10 18:19:02 -0700

    assignment-intro-crud: completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.first.title = "SPAM"
+
+    @posts.each_with_index do |post, i|
+      post.title = "SPAM" if i % 5 === 0
+    end
   end
 
   def show
